13f061f0406df8567a083bc3ddf7ec72fbcacab9
スティーブンさん <underyourcover@icloud.com>
2019-06-03 20:54:12 -0700

    updated formatting in client side files

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -5,10 +5,10 @@ class Search extends React.Component {
     super(props);
     this.state = {
       term: ''
-    }
+    };
   }
 
-  onChange (e) {
+  onChange(e) {
     this.setState({
       term: e.target.value
     });
@@ -19,11 +19,14 @@ class Search extends React.Component {
   }
 
   render() {
-    return (<div>
-      <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
-      <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    return (
+      <div>
+        <h4>Add more repos!</h4>
+        Enter a github username:{' '}
+        <input value={this.state.terms} onChange={this.onChange} />
+        <button onClick={this.search}> Add Repos </button>
+      </div>
+    );
   }
 }
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,23 +7,24 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
-    }
-
+    };
   }
 
-  search (term) {
+  search(term) {
     console.log(`${term} was searched`);
     // TODO
   }
 
-  render () {
-    return (<div>
-      <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
-    </div>)
+  render() {
+    return (
+      <div>
+        <h1>Github Fetcher</h1>
+        <RepoList repos={this.state.repos} />
+        <Search onSearch={this.search.bind(this)} />
+      </div>
+    );
   }
 }
 
