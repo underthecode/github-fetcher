ff913137aa58a96502e09058add2d91c4d2ea74a
スティーブンさん <underyourcover@icloud.com>
2019-06-03 23:04:08 -0700

    configured repoSchema

--- database/index.js
+++ database/index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({});
+let repoSchema = mongoose.Schema({
+  repos_id: { type: String, unique: true },
+  repos_full_name: String,
+  repos_html_url: String,
+  repos_description: String,
+  repos_updated_at: String
+});
 
 let Repo = mongoose.model('Repo', repoSchema);
 
