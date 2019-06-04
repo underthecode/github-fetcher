fd4a3418365bcf4c9d8f987d9612e7dd2d88f5ff
スティーブンさん <underyourcover@icloud.com>
2019-06-03 23:53:49 -0700

    updated getReposByUsername

--- server/helpers/github.js
+++ server/helpers/github.js
@@ -1,14 +1,24 @@
 const request = require('request');
 const config = require('../config/config');
 
-let getReposByUsername = () => {
+let getReposByUsername = (user, callback) => {
   let options = {
-    url: '',
+    url: `https://api.github.com/users/${user}/repos`,
     headers: {
       'User-Agent': 'request',
       Authorization: `token ${config.TOKEN}`
     }
   };
+
+  // found code snippet inside of request NPM docs
+  request(options, (err, response, body) => {
+    if (err && !response.statusCode === 201) {
+      callback(err, null);
+    } else {
+      const repos = JSON.parse(body);
+      callback(null, repos);
+    }
+  });
 };
 
 module.exports.getReposByUsername = getReposByUsername;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const helpers = require('./helpers/github');
+
 let app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.post('/repos', function(req, res) {});
+app.post('/repos', function(req, res) {
+  const user = req.body.user;
+  helpers.getReposByUsername(user, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      // post GitHub API data to database
+      // console.log(data)
+    }
+  });
+  res.sendStatus(201);
+});
 
 app.get('/repos', function(req, res) {});
 
